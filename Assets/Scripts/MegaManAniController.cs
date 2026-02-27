470b0b0b901309bb89f74bf5f824409b78fc75c0
Shade2231S <matthewmcculloch149@gmail.com>
2026-02-27 16:47:24 -0400

    Problem solving

--- Assets/Scripts/MegaManAniController.cs
+++ Assets/Scripts/MegaManAniController.cs
@@ -5,6 +5,7 @@ public class MegaManAniController : MonoBehaviour
 {
     public Animator animator;
     public Vector2 move;
+    private int MoveInputHashx = Animator.StringToHash("Moveinputx");
     public void Start()
     {
         animator = GetComponent<Animator>();
@@ -12,5 +13,8 @@ public class MegaManAniController : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         move = context.ReadValue<Vector2>();
+        animator.SetFloat(MoveInputHashx, move.x);
+
+
     }
 }
